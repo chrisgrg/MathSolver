3ca0e6d249f0d309f0b6732950e20f9a200f3b33
George <chrisgrg@gmail.com>
2018-08-04 18:58:57 +0530

    Commit changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+// this is new
+
 const App = () => <div className="App" />;
 
 export default App;
--- src/Components/OutputField.js
+++ src/Components/OutputField.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 
+//this is test
 class asdasd extends Component {
   state = {};
   render() {
